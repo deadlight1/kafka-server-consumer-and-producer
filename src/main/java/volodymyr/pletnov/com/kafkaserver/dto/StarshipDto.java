90af8bf20e27a2c8eda9500ac964e533ac985055
Vladimir <deadlight967@gmail.com>
2020-06-30 18:49:12 +0300

    create configs and dtos

---
+++ src/main/java/volodymyr/pletnov/com/kafkaserver/dto/StarshipDto.java
@@ -0,0 +1,12 @@
+package volodymyr.pletnov.com.kafkaserver.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class StarshipDto extends AbstractDto {
+
+    private String name;
+    private String model;
+}
